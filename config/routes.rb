c340c7256144535802a2d87fa1832b80ccf98376
soe-j <soeda@val.co.jp>
2017-06-01 20:34:07 +0900

    correct route setting

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'butas#index'
-  get 'butas/:name', 'butas#show'
+  get 'butas/:name', to: 'butas#show'
 end
