140053882763da6789be450d2f25846e5121f1ed
soe-j <soeda@val.co.jp>
2017-06-01 23:19:06 +0900

    output level

--- app/models/pig.rb
+++ app/models/pig.rb
@@ -1,3 +1,18 @@
 class Pig < ApplicationRecord
   has_many :topping_logs
+
+  def level
+    case self.topping_logs.sum(:mashi)
+    when 0...5
+      1
+    when 5...10
+      2
+    when 10...30
+      3
+    when 30...Float::INFINITY
+      4
+    else
+      0
+    end
+  end
 end
