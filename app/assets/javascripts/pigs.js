5b00e183c5e362fcdd93d88654c1faff968758b6
soe-j <soeda@val.co.jp>
2017-06-03 12:54:44 +0900

    get only first log

--- app/assets/javascripts/pigs.js
+++ app/assets/javascripts/pigs.js
@@ -3,15 +3,13 @@ $(document).on('turbolinks:load', function(){
   var offset = gon.offset;
 
   setInterval(function () {
-    $.getJSON('/pigs/' + gon.pig_name + '/topping_logs?offset=' + offset , function (logs) {
-      logs.forEach(function (log) {
+    $.getJSON('/pigs/' + gon.pig_name + '/topping_logs/first?offset=' + offset , function (log) {
+      if (log) {
         var imgSrc = '/assets/' + gon.pig_name + '-' + gon.pig_level + '-' + log.topping_name + '.gif';
         pigImage.attr('src', imgSrc);
-      });
-
-      offset += logs.length;
-      console.log(logs);
+        offset += 1;
+      }
     });
-    console.log('hey');
+    console.log(log);
   }, 1000);
 });
