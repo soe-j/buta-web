eef866ffcd4fa5eb35f067ee30a484ab61d68941
soe-j <soeda@val.co.jp>
2017-06-03 13:29:27 +0900

    loading stop and eating if mashimashi

--- app/assets/javascripts/pigs.js
+++ app/assets/javascripts/pigs.js
@@ -1,15 +1,33 @@
 $(document).on('turbolinks:load', function(){
   var pigImage = $('#pig-image');
+  var defaultImageSrc = pigImage.attr('src');
   var offset = gon.offset;
+  var more = 0;
+  var topping = "";
 
   setInterval(function () {
-    $.getJSON('/pigs/' + gon.pig_name + '/topping_logs/first?offset=' + offset , function (log) {
-      if (log) {
-        var imgSrc = '/assets/' + gon.pig_name + '-' + gon.pig_level + '-' + log.topping_name + '.gif';
-        pigImage.attr('src', imgSrc);
-        offset += 1;
-      }
-    });
-    console.log(log);
-  }, 1000);
+    if (more > 0) {
+      eat();
+      more -= 1;
+    } else {
+      $.getJSON('/pigs/' + gon.pig_name + '/topping_logs/first?offset=' + offset , function (log) {
+        if (log) {
+          topping = log.topping_name;
+          eat();
+          offset += 1;
+          more = log.mashi - 1;
+        }
+      });
+    }
+  }, 1500);
+
+  var eat = function () {
+    var imgSrc = '/assets/' + gon.pig_name + '-' + gon.pig_level + '-' + topping + '.gif';
+    pigImage.attr('src', imgSrc);
+    console.log(topping);
+    setTimeout(function(){
+      pigImage.attr('src', defaultImageSrc);
+      console.log('default');
+    }, 1000);
+  };
 });
