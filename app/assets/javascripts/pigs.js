e9663f89c1c59a132639b4dd095fe0d06403df0a
soe-j <soeda@val.co.jp>
2017-06-03 12:28:29 +0900

    js changes image

--- app/assets/javascripts/pigs.js
+++ app/assets/javascripts/pigs.js
@@ -1,8 +1,14 @@
 $(document).on('turbolinks:load', function(){
+  var pigImage = $('#pig-image');
   var offset = gon.offset;
 
   setInterval(function () {
     $.getJSON('/pigs/' + gon.pig_name + '/topping_logs?offset=' + offset , function (logs) {
+      logs.forEach(function (log) {
+        var imgSrc = '/assets/' + gon.pig_name + '-' + gon.pig_level + '-' + log.topping_name + '.gif';
+        $('#pig-image').attr('src', imgSrc);
+      });
+
       offset += logs.length;
       console.log(logs);
     });
